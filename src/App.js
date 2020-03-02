589c98aef405404617514818d5b9050091a3d362
henry hawm <henry.hawm@gmail.com>
2020-03-02 09:35:07 -0600

    redirect

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./pages/About";
 import Discover from "./pages/Discover";
-import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
@@ -14,7 +13,7 @@ function App() {
             <div>
                 <Navbar />
                 <Wrapper>
-                    <Route exact path="/" component={About} />
+                    <Route exact path="/react-portfolio" component={About} />
                     <Route exact path="/about" component={About} />
                     <Route exact path="/portfolio" component={Discover} />
                 </Wrapper>
