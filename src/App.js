bbe8c4c78d6b7ce7e6a5337c9d55ab71a9e20afd
henry hawm <henry.hawm@gmail.com>
2020-03-02 02:27:44 -0600

    retry

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ function App() {
                 <Wrapper>
                     <Route exact path="/" component={About} />
                     <Route exact path="/about" component={About} />
-                    <Route exact path="/discover" component={Discover} />
-                    <Route exact path="/search" component={Search} />
+                    <Route exact path="/portfolio" component={Discover} />
                 </Wrapper>
                 <Footer />
             </div>
